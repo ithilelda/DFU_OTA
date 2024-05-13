f2749ef2a6269447e6d5eaad292023b52a785f97
ithilelda <ithilelda@gmail.com>
2024-05-13 20:58:24 +0800

    beacause flash is reset to FFs and set to 0, I change the boot flag direction.

--- src/main.c
+++ src/main.c
@@ -13,9 +13,9 @@ __attribute__((aligned(4))) uint32_t MEM_BUF[BLE_MEMHEAP_SIZE / 4];
 int main()
 {
     SetSysClock(CLK_SOURCE_PLL_60MHz);
-    uint32_t boot_app;
-    EEPROM_READ(EEPROM_DATA_ADDR, &boot_app, sizeof(boot_app));
-    if(boot_app == 1) // only the number 1 will boot the app.
+    uint32_t boot_ota;
+    EEPROM_READ(EEPROM_DATA_ADDR, &boot_ota, sizeof(boot_ota));
+    if(!boot_ota) // only the number 0 will boot the app because the flash will be erased to 1's. You have to actively set it to be 0, so 0 means set.
     {
         jumpApp();
     }
