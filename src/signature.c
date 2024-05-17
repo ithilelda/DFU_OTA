fa1bfd216f7634d5bfbf8b9b5d1a7f77549708ad
ithilelda <ithilelda@gmail.com>
2024-05-17 09:38:02 +0800

    settling on hmac. not gonna deal with ecc.

--- src/signature.c
+++ src/signature.c
@@ -12,12 +12,12 @@
  */
 bStatus_t VerfiySignature(uint8_t *pData, uint8_t len, uint8_t *pSignature, uint8_t *pKey)
 {
-#if defined SIGNATURE_ED25519
-    return ed25519VerifySignature(pKey, pData, len, NULL, 0, 0, pSignature);
-#elif defined SIGNATURE_HMAC256
+#if SIGNATURE_ALGO == SIG_HMAC256
     uint8_t hmacBuffer[SHA256_DIGEST_SIZE];
     hmacCompute(SHA256_HASH_ALGO, pKey, SIGNATURE_KEY_LEN, pData, len, hmacBuffer);
     return !tmos_memcmp(hmacBuffer, pSignature, SHA256_DIGEST_SIZE);
+#elif SIGNATURE_ALGO == SIG_ED25519
+    return ed25519VerifySignature(pKey, pData, len, NULL, 0, 0, pSignature);
 #else
     return 1;
 #endif
